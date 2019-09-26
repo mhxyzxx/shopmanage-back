41663b3ceeb068693c0a8a022301329b01e86272
mhxyzxx <mhxyzxx@163.com>
2019-09-27 00:25:36 +0800

    完成登录组件的路由配置

---
+++ src/components/login.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+     login组件------
+ </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login.vue'
+// import Login from '../components/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
-    routes: [{
-        path: '/',
-        name: 'home'
-    }]
+  routes: [{
+    path: '/',
+    name: 'home'
+  }, {
+    name: 'login',
+    path: '/login',
+    component: Login
+  }]
 })

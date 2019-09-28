e5f159ccf533589d53e9e582f067e780bd88e605
mhxyzxx <mhxyzxx@163.com>
2019-09-28 11:20:26 +0800

    处理axios请求的响应

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,17 +1,26 @@
 <template>
   <!-- 注意为了方面调整样式在最外部包了一个div -->
-    <div class="login-wrap">
-        <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
-            <h2>用户登录</h2>
-            <el-form-item label="用户名">
-                <el-input v-model="formdata.username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input v-model="formdata.password"></el-input>
-            </el-form-item>
-            <el-button class="login-btn" type="primary">登录</el-button>
-        </el-form>
-    </div>
+  <div class="login-wrap">
+    <el-form
+      class="login-form"
+      label-position="top"
+      label-width="80px"
+      :model="formdata"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button
+        class="login-btn"
+        type="primary"
+        @click.prevent="handleLogin"
+      >登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -25,6 +34,22 @@ export default {
         username: '',
         password: ''
       }
+    }
+  },
+  methods: {
+    handleLogin () {
+      this.$http.post('login', this.formdata).then((res) => {
+        console.log(res)
+        const { data: { data, meta: { msg, status } } } = res
+        if (status == 200) {
+          this.$message({
+            message: msg,
+            type: 'success'
+          })
+        } else {
+          this.$message.error(msg)
+        }
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/base.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+// 配置基础路径
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false

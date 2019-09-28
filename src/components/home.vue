f3f6357e3feb5c6637ef6df8035b0d9f241e3d69
mhxyzxx <mhxyzxx@163.com>
2019-09-28 19:22:36 +0800

    首页布局及样式调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,21 +1,37 @@
 <template>
-  <div>
-    渲染home组件
-  </div>
+  <el-container class="container">
+    <el-header class="header">Header</el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-  components: {
+  export default {
+    components: {
 
-  },
-  data () {
-    return {
+    },
+    data () {
+      return {
 
+      }
     }
   }
-}
 </script>
 
 <style>
+.container {
+  height: 100%;
+}
+.header {
+  background-color: red;
+}
+.aside {
+  background-color: pink;
+}
+.main {
+  background-color: yellow;
+}
 </style>

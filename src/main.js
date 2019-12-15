67522bdedaa7b691b494c536435b6f6aff79555d
mhxyzxx <mhxyzxx@163.com>
2019-12-15 16:17:19 +0800

    修改了main.js文件

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import moment from 'moment'
 // 把不是Vue插件axios变成Vue插件
 // Vue.use(axios)
 // 我们可以去Vue官网查看编写插件的API文档
-Vue.use(HttpServer);
+Vue.use(HttpServer)
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
@@ -21,18 +21,18 @@ Vue.config.productionTip = false
 // 全局过滤器，处理日期格式
 // 这个全局过滤器没有设置自己的参数，参数是在函数里面传的
 Vue.filter('fmtdate', v => {
-    return moment(v).format('YYYY-MM-DD')
-});
+  return moment(v).format('YYYY-MM-DD')
+})
 
 // 注册全局公共组件
 // Vue.component('comBread', ComBread);
 // 也可以使用下面的写法
-Vue.component(ComBread.name, ComBread);
+Vue.component(ComBread.name, ComBread)
 
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router,
-    components: { App },
-    template: '<App/>'
-});
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
